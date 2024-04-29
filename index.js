76af58ad8974c854f8f3b5034f79aae6b4a6b29c
Adrian Sobota <sobotamatuszak@gmai.com>
2024-04-29 16:08:46 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0
+    this.items = []
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.max(...this.items) // Test de la clase de hoy
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return Math.min(...this.items) // Test de la clase de hoy
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0
+    }
+    return this.items.reduce((sum, current) => sum + current, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.sum() / this.length
+  }
 }
 
-module.exports = SortedList;
+module.exports = SortedList
+
